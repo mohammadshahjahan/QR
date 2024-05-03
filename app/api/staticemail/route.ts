6a98f13a8006af342dabd990c758c09b2a86f05a
Shahjahan <md.shahjahan786pubg@gmail.com>
2024-05-03 16:19:50 +0530

    added static email api integertion with static email form in Frontend

--- app/api/staticemail/route.ts
+++ app/api/staticemail/route.ts
@@ -4,12 +4,20 @@ import prisma from "@/libs/prismadb";
 export async function POST(req: NextRequest) {
   const body = await req.json();
   try {
-    const { goToMail, name, userId } = body;
+    const { goToMail, name, email } = body;
+    const u = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (!u) {
+      throw new Error("not found user");
+    }
     const qr = await prisma.staticEmail.create({
       data: {
         goToMail,
         name,
-        userId,
+        userId: u.id,
       },
     });
     return NextResponse.json(qr);
--- components/StaticEmailForm.tsx
+++ components/StaticEmailForm.tsx
@@ -3,16 +3,26 @@
 import React, { useState } from "react";
 import { Button } from "./ui/button";
 import QRCode from "qrcode";
+import axios from "axios";
+import { useSession } from "next-auth/react";
 
 const StaticEmailForm = () => {
+  const session = useSession();
   const [qr, setQr] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const generate = () => {
+  const generate = async () => {
     if (!email) {
       alert("NO input");
       return;
     }
     QRCode.toDataURL(`mailto:${email}`).then(setQr);
+    const r = await axios.post("/api/staticemail", {
+      goToMail: email,
+      name,
+      email: session.data?.user?.email,
+    });
+    console.log(r);
   };
   return (
     <div className="flex justify-center items-center h-full p-5 ">
@@ -22,7 +32,11 @@ const StaticEmailForm = () => {
             <strong>Generate your Static Email QR!</strong>
             <div className="grid grid-cols-2">
               <label className="p-2">Name</label>
-              <input placeholder="" className="border-solid border-2 m-2" />
+              <input
+                placeholder=""
+                className="border-solid border-2 m-2"
+                onChange={(e) => setName(e.target.value)}
+              />
               <label className="p-2">Email</label>
               <input
                 onChange={(e) => setEmail(e.target.value)}
